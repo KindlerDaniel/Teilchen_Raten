34cd311a1fd5cf580573adeeeeb3801b2203e83d
KindlerDaniel <108432156+KindlerDaniel@users.noreply.github.com>
2022-06-30 11:34:37 +0200

    Update Main_class.java

--- src/Main_class.java
+++ src/Main_class.java
@@ -1,4 +1,6 @@
-import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -6,39 +8,50 @@ import java.nio.file.Path;
 import java.util.stream.IntStream;
 
 
-public class Main_class {
+public class MainClass {
 
     // Read the initial state of the Board from JSON file.
     // Instantiate and connect Board and Observer and GUI.
-    public static void main(String[] args){
-        int[][] initial_board = read_world("world_0");
-        int height = initial_board.length;
-        int width = initial_board[0].length;
+    public static void main(final String[] args) throws JSONException {
+        int[][] initialBoard = read_world("world_0");
+        int height = initialBoard.length;
+        int width = initialBoard[0].length;
         Board board = new Board(height, width);
         Observer observer = new Observer(height, width);
         observer.connect_board(board);
-        board.initialize(observer, initial_board);
+        board.initialize(observer, initialBoard);
         new GUI(height, width, board, observer);
     }
 
     // Reads from a JSON file named worlds within the same directory.
     // Outputs a matrix representation of encoded board.
-    public static int[][] read_world(String world_name){
+    public static int[][] read_world(final String worldName) throws JSONException {
         String raw = "";
-        Path path = Path.of(System.getProperty("user.dir") + "\\worlds");
-        try{
+        Path path = Path.of(System.getProperty("user.dir") + "/worlds");
+        try {
             raw = Files.readString(path);
         } catch (IOException e) {
             e.printStackTrace();
         }
         JSONObject worlds = new JSONObject(raw);
-        JSONArray world = worlds.getJSONArray(world_name);
+        JSONArray world = worlds.getJSONArray(worldName);
         JSONArray shape = world.getJSONArray(0);
         JSONArray triples = world.getJSONArray(1);
         int[][] matrix = new int[shape.getInt(0)][shape.getInt(1)];
-        IntStream.range(0, triples.length()).mapToObj(i -> triples.getJSONArray(i)).
-        forEach(t -> matrix[t.getInt(1)][t.getInt(0)] = t.getInt(2));
-
+        IntStream.range(0, triples.length()).mapToObj(i -> {
+                    try {
+                        return triples.getJSONArray(i);
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).
+                forEach(t -> {
+                    try {
+                        matrix[t.getInt(1)][t.getInt(0)] = t.getInt(2);
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
         return matrix;
     }
 }
